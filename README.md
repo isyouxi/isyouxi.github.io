8d18d1dc0e8c3fb4c5addc826424e8495e793001
isyouxi <is_youxi@163.com>
2016-10-10 14:22:19 +0800

    用于测试

--- README.md
+++
@@ -1,4 +0,0 @@
-### isyouxi.github.io
-
-- 这个是关于我的个人网站: isyouxi.xyz
-- 欢迎访问,提意见.
--- js/application.js
+++
@@ -1,4 +0,0 @@
-// Disable link clicks to prevent page scrolling
-$(document).on('click', 'a[href="#fakelink"]', function (e) {
-  e.preventDefault();
-});
