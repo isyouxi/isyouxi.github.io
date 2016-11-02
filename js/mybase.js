e088fa432ec4e42b1b838d840a3476b3eedcc13b
isyouxi <is_youxi@163.com>
2016-11-02 16:13:09 +0800

    haha

--- js/mybase.js
+++ js/mybase.js
@@ -8,44 +8,55 @@ var curr_Window_with;
 var currScreenMode = '';//large,small
 
 function init() {
-
-
     // curr_Window_height = screen.availHeight;  //- 可用的屏幕宽度
     // curr_Window_with = screen.availWidth;// - 可用的屏幕高度
-
     curr_Window_with = document.body.clientWidth;
     curr_Window_height = document.body.clientHeight;
     updateScreenSet();
+    setVerticalBar();
     setClick();
-
 }
 
 
-function setClick() {
+function setVerticalBar() {
 
+
+    var navs = [];
+    for (var i = 1; i < 5; i++) {
+        navs[i - 1] = $('[data-number =   ' + i + ']');
+
+      //  $("p").focus(); 或$("p").focus(fn)
+
+        navs[i - 1].mousemove(function (e) {
+            var childs = $(this).children();
+            $(this).children("div.li-dot").hide();
+            $(this).children("div.li-label").show();
+        });
+
+        navs[i - 1].mouseleave(function (e) {
+            var childs = $(this).children();
+            $(this).children("div.li-label").hide();
+            $(this).children("div.li-dot").show();
+        });
+    }
+
+    console.log('当前i:', navs);
+}
+
+function setClick() {
     /** 菜单点击 **/
     $("#item-menus").click(function () {
         console.log("onclick");
 
         if ($('body').css('-webkit-transform') != 'none') {
-            //var currTrans = $('body').css('-webkit-transform').split(/[()]/)[1];
-            // console.log(currTrans);
-            // var curr_translateX = currTrans.split(',')[4];
-            // console.log(curr_translateX);
             $('body').css('-webkit-transform', 'none');
         } else {
             $('body').css('transform', 'translateX(-15.625rem)');
         }
-
-
     });
 }
 
 function sizeChange() {
-    //console.log("sizeChange")
-    //curr_Window_height = screen.availHeight;  //- 可用的屏幕宽度
-    //curr_Window_with = screen.availWidth; // - 可用的屏幕高度
-
     curr_Window_with = document.body.clientWidth;
     curr_Window_height = document.body.clientHeight;
     updateScreenSet();
@@ -61,7 +72,7 @@ function updateScreenSet() {
         console.log('change set To large');
         currScreenMode = 'large';
     } else {
-        //console.log("no change set curr_Window_with:" + curr_Window_with + "||currScreenMode:" + currScreenMode);
+
         return;
     }
 
@@ -73,6 +84,5 @@ function updateScreenSet() {
     } else {
         $('.mobile-top-bar').show();
         $('.normal-top-bar').hide();
-
     }
 }
